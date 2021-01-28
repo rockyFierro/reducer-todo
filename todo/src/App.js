4ec762b84b9cfa719ec4a1d3d41b8bcefb80b661
rockyFierro <finnslighthouse@gmail.com>
2021-01-28 12:41:03 -0800

    addding comments before doing more.

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,7 +1,66 @@
+import React, { useReducer, useState } from "react";
+//import useReducer, does magic dispatch thing
+
+//reducer takes state, an action that describes an event and uses that to return a mutated copy of original state.
+function reducer(state, action) {
+  switch (action.type) {
+    case "ADD_TODO":
+      return {
+        todos: [...state.todos, { text: action.text, completed: false }],
+      };
+    case "TOGGLE_TODO":
+      return {
+        todos: state.todos.map((task, index) =>
+        index === action.index ? { ...task, completed: !task.completed } : task
+      ),
+      
+      };
+      case "DELETE_TODO":
+        return {
+          todos: state.todos.filter((task) => !task.completed)
+        };
+
+    default:
+      return state;
+  }
+}
 
 function App() {
+  //useReducer takes in a reducer, this one is a root and controls everything, it also takes in an initialState.
+  const [{ todos }, dispatch] = useReducer(reducer, { todos: [] });
+  const [text, setText] = useState();
+
   return (
     <div className="App">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          dispatch({ type: "ADD_TODO", text });
+          setText("");
+        }}
+      >
+        <input value={text} onChange={(e) => setText(e.target.value)} />
+        <button>Submit</button>
+        <br />
+        <button
+        onClick={(e) => {
+          e.preventDefault()
+          dispatch({type: "DELETE_TODO"})
+        }}
+        
+        >Clear Completed</button>
+      </form>
+      {todos.map((task, index) => (
+        <div
+          key={task.text}
+          onClick={() => dispatch({ type: "TOGGLE_TODO", index })}
+          style={{
+            textDecoration: task.completed ? "line-through" : ""
+          }}
+        >
+          {task.text}
+        </div>
+      ))}
     </div>
   );
 }
---
+++ todo/src/components/TodoForm.jsx
@@ -0,0 +1,10 @@
+import React, { useReducer } from 'react'
+import { todosReducer } from '../reducers/todoReducer'
+
+export const TodoForm = () => {
+  return(
+    <form>
+      
+    </form>
+  )
+}
